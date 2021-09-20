d667a9672debeddf163f68df75281ba0ec64161b
Veronica Mascareño <vmascareno@uabc.edu.mx>
2021-09-19 18:35:38 -0700

    fix: agregué las rutas faltantes

--- database/seeders/RoutesTableSeeder.php
+++ database/seeders/RoutesTableSeeder.php
@@ -16,9 +16,14 @@ class RoutesTableSeeder extends Seeder
     public function run()
     {
         Route::insert([
-            // Mass insert
             [
                 'name' => 'Mexicali-Tecate-Tijuana-Rosarito-Ensenada',
+                'distance' => 289.78,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'Ensenada-Rosarito-Tijuana-Tecate-Mexicali',
                 'distance' => 289.78,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
@@ -30,13 +35,31 @@ class RoutesTableSeeder extends Seeder
                 'updated_at' => Carbon::now(),
             ],
             [
+                'name' => 'Tecate-Mexicali',
+                'distance' => 129.75,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
                 'name' => 'Mexicali-Tecate-Ensenada',
                 'distance' => 269.69,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ],
             [
+                'name' => 'Ensenada-Tecate-Mexicali',
+                'distance' => 269.69,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
                 'name' => 'Mexicali-Tijuana',
+                'distance' => 171.56,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'Tijuana-Mexicali',
                 'distance' => 171.56,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
